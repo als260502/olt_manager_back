dddb0f1c3d99d083031fde7b6765ae4303c96226
Andre <als260602@msn.com>
2019-10-30 15:58:12 -0300

    add:configurando bd de usuario

---
+++ src/database/migrations/20191030125406-create-user.js
@@ -0,0 +1,41 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Users");
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routes')
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
 
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
 
 module.exports = app;
-
